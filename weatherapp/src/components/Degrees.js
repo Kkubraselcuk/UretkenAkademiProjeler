085416a08896ba3a9de6b9534b26fc98ac6dbd38
kkubraselcuk <kkubra.selcuk@gmail.com>
2023-03-15 14:45:11 +0300

    weatherapp upload

---
+++ weatherapp/src/components/Degrees.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Degrees extends Component {
+  render() {
+    return (
+      <>
+      3 Birimde Sıcaklık Ölçümü
+      </>
+    )
+  }
+}
---
+++ weatherapp/src/components/Home.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import Degrees from './Degrees';
+import Celcius from './Celcius';
+import Fahrenheit from './Fahrenheit';
+import Kelvin from './Kelvin';
+import 'bootstrap';
+export default class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            celcius: 0,
+            fahrenheit: 0,
+            kelvin: 0,
+        }
+    }
+    increaseDegree = () =>{
+        this.setState({
+            celcius: this.state.celcius+1,
+            fahrenheit: (this.state.celcius*1.18)+32,
+            kelvin: this.state.celcius+273.15,
+        })
+    }
+    render() {
+        return (
+            <>
+
+                <div className="container">
+                    <h1>Hava Nasıl</h1>
+                    Şuan Sıcaklık: {this.state.celcius} derece<br/><br/>
+                        <button type="button" className="btn btn-success btn-sm" onClick={()=>this.increaseDegree()}>Sıcaklık Arttır</button><br/><br/>
+                        <div className='row'>
+                            <div className='col'>
+                                <Degrees/>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col'>
+                                <Celcius celciusValue={this.state.celcius}/>
+                            </div>
+                            <div className='col'>
+                                <Fahrenheit fahrenheitValue={this.state.fahrenheit}/>
+                            </div>
+                            <div className='col'>
+                                <Kelvin kelvinValue={this.state.kelvin}/>
+                            </div>
+                        </div>
+                </div>
+            </>
+        )
+    }
+}
